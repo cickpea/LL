d3df0f78766eda5373e73b6556c401b4e0ffab26
“cickpea” <laboouur@outlook.com>
2024-03-26 10:50:53 -0300

    First version with Freinds

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include "foodlist.h"
+#include <iostream>
+# include <string>
+
+using namespace std;
+
+int main() {
+    FoodList myList;
+    char choice;
+
+    while (true) {
+        cout << "    " << endl;
+        cout << "'a': add " << endl;
+        cout << "'s': search" << endl; 
+        cout << "'p': print" << endl;
+        cout << "'d': delete" << endl; 
+        cout << "'e': check for empty" << endl;  
+        cout << "'n': size or count" << endl; 
+        cout << "'c': clear the strucutre entirely" << endl; 
+        cout << "'q': quit" << endl; 
+        cout << "Your choice: ";
+        cin >> choice;
+        // std::cin.ignore();
+
+        if (choice == 'q') {
+            break;
+        }
+
+        switch (choice) {
+        case 'a': {
+            Food* newFood = new Food();
+            newFood->read();
+            myList.sortedInsert(newFood);
+            cout << "Item added and sorted." << std::endl;
+            break;
+        }
+        case 's': {
+            string searchName;
+            cout << "Enter the name of the food item to search: ";
+            cin >> searchName;
+            Food* foundItem = myList.search(searchName);
+            if (foundItem != NULL) {
+                cout << "\nFood item found:" << std::endl;
+                foundItem->write();
+            }
+            else {
+                cout << "\nFood item not found." << std::endl;
+            }
+            break;
+        }
+        case 'p': {
+            cout << "\nFood items in the list:" << std::endl;
+            myList.display();
+            break;
+        }
+        case 'd': {
+            string deleteName;
+            cout << "Enter the name of the food item to delete: ";
+            cin >> deleteName;
+            myList.deleteItem(deleteName);
+            break;
+        }
+        case 'e': {
+            if (myList.empty()) {
+                cout << "List is empty." << std::endl;
+            }
+            else {
+                cout << "List is not empty." << std::endl;
+            }
+            break;
+        }
+        case 'n': {
+            cout << "The count is " << myList.count() << std::endl;
+            break;
+        }
+        case 'c': {
+            myList.clear();
+            cout << "List cleared." << std::endl;
+            break;
+        }
+        default:
+            cout << "\nInvalid choice." << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
